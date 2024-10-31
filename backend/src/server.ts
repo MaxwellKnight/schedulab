4799343f7e6f4b8d80cc5c7c4ec07d2bd102511d
Maxwell Knight <maxwell.knight98@gmail.com>
2024-10-31 17:44:14 +0200

    updated package-lock & cors in backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,10 @@ app.use(cors({
 	origin: 'http://localhost:5173',
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization']
+	allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Credentials'],
+	exposedHeaders: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Credentials'],
+	preflightContinue: false,
+	optionsSuccessStatus: 204
 }));
 
 //routes
